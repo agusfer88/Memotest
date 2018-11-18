27d4dbb27c24c92c60825eb5aedb5cd8ecbe8e0d
unknown <agusfernandez1988@gmail.com>
2018-11-18 17:41:01 -0300

    inicio de planteo para comparar cartas

--- scripts.js
+++ scripts.js
@@ -1,36 +1,39 @@
-
+var jugadores = []; 
   // planteo para login
 
-  var jugadores = []; 
-  var nombre;
-  var intentos;
-  var nivel; 
 
-  var jugador =  {
-  nombre: nombre.val(),
-  nivel: nivel.val(),
-  intentos: intentos.val()
-}
 
-// agregar contenido a la propiedad nombre de la variable jugador
-  function guardarNombre (){
-
-    // $("#nombre").on("click", function(){
-        nombre = $("#nombre").val();
-
-        jugador.push(nombre)
-   
-
-        // Modificar el contenido de <span class="jugador">
-
-   
+  function guardarJugador (){
+    var nombre = $("#nombre").val();
+    var nivel =$()
+    var intentos= $(".intentos").val();
   
-        
-        
-    // })
- };
+    var jugador =  {
+      nombre: nombre,
+      nivel: nivel,
+      intentos: intentos
+      }
+  
+      nivel = $(".facil").on("click", function() { 
+        nivel = 18
+      
+    });
+    nivel = $(".intermedio").on("click", function() { 
+        nivel = 12
+    });
+    nivel =  $(".experto").on("click", function() { 
+        nivel = 9
+    });
 
 
+    jugadores.push(jugador);
+ console.log(jugadores);
+  }
+
+
+function guardarNivel(){
+
+}
 
     
 
@@ -45,31 +48,66 @@
 
 // ordena las cartas de manera random
 const imagenes = [
-{src: "img/epelante.jpg"},
-{src: "img/alce.jpg"},
-{src: "img/nena.jpg"},
-{src: "img/peces.jpg"},
-{src: "img/unichancho.jpg"},
-{src: "img/zapas.jpg"},
-{src: "img/epelante.jpg"},
-{src: "img/nena.jpg"},
-{src: "img/peces.jpg"},
-{src: "img/unichancho.jpg"},
-{src: "img/zapas.jpg"},
-{src: "img/alce.jpg"}
+{src: "img/epelante.jpg", id:"epelante1"},
+{src: "img/alce.jpg", id:"alce1"},
+{src: "img/nena.jpg", id:"nena2"},
+{src: "img/peces.jpg", id:"peces2"},
+{src: "img/unichancho.jpg", id:""},
+{src: "img/zapas.jpg", id:"zapas2"},
+{src: "img/epelante.jpg", id:"epelante2"},
+{src: "img/nena.jpg", id:"nena1"},
+{src: "img/peces.jpg", id:"peces1"},
+{src: "img/unichancho.jpg", id:"unichancho1"},
+{src: "img/zapas.jpg", id:"zapas1"},
+{src: "img/alce.jpg", id:"alce2"}
 ];
 var LosDivCard= $(".carta");
 
 for (let i = 0; i < LosDivCard.length; i++) {
 var ruta = imagenes[i].src;
-   var imgDestapada= $("<img class='destapada' src="+ ruta +">");
+var id = imagenes[i].id;
+   var imgDestapada= $("<img class='destapada front' src="+ ruta +" id="+id+">");
    LosDivCard.eq(i).append(imgDestapada);
 }
 
 imagenes.sort(function(){
     return Math.random() - 0,5;
 });
-var clicks = 0
+
+// PLANTEO PARA COMPARAR CARTAS
+var clicks = 0;
+var carta1 = null;
+var carta2 = null;
+var intentos = clicks ++
+$(".carta").on('click', function(){
+
+
+ const imgSrc = $('this').children($(".front")).attr('src')
+    
+
+    const id = $('this').attr('id')
+    if (clicks == 1){
+        carta1 = {
+            src: imgSrc,
+            id: id
+        }
+    }
+    else {
+        carta2 = {
+            src: imgSrc,
+            id: id
+        }
+   
+    if (carta1.src == carta2.src && carta1.id != carta2.id){
+        console.log("iguales")
+    }
+    else{
+        console.log('distintos')
+    }
+    clicks = 0
+}
+
+});
 
 
 ///Cuando clickea la carta 
@@ -94,18 +132,19 @@ var clicks = 0
 
 
 
-// cambio la clase del div tablero para que se muestre(podria usar toggle)
+
 
 
 
 
 // planteo para contar intentos
+
+
 /// consultar tengo que crear una variable que me aumente por cada click = Click++
 //entonces los intentos van a bajar de dos en dos segun los clicks que yo haga (ver ejercicio que iba iterando de a dos)
 
 
 
-// metodo data index es para comparar las cartas y saber si son iguales
 
 
 
@@ -117,5 +156,5 @@ function verCarta(){
     })
 }
 
-// guardarNombre();
+
 verCarta();
